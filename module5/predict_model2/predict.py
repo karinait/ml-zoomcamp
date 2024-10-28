790b230d3d7b3317a5a03846fa9f2a8801ec12d9
karinait <karina.in.it@gmail.com>
2024-10-28 16:46:09 -0400

    homework 5 ready

---
+++ module5/predict_model2/predict.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+model_file="model2.bin"
+with open(model_file, 'rb') as f_in:
+    model = pickle.load(f_in)
+
+app=Flask('churn_predict')
+
+dv_file="dv.bin"
+with open(dv_file, 'rb') as f_in:
+    dv = pickle.load(f_in)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	customer = request.get_json()
+	X = dv.transform(customer)
+	y_pred_proba=model.predict_proba(X)[0,1]
+	churn = y_pred_proba >= 0.5
+
+	result = {
+		'churn_probability': y_pred_proba,
+		'churn': bool(churn)
+	}
+	return jsonify(result)
+
+if __name__ == "__main__":
+	app.run(debug=True,  host='0.0.0.0', port=9696)
